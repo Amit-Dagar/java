2a10298332877512141622939f0a787ae5b70b4a
Amit Dagar <32691543+FreeCoder111@users.noreply.github.com>
2019-01-13 15:28:46 +0530

    Create SquareRoot.java

---
+++ SquareRoot.java
@@ -0,0 +1,19 @@
+/*
+* This a java program to calculate Square Root using java.lang.Math class.
+* Java program with multiple Statement
+* More Java Statements
+* This code computes square root
+* @ Amit Dagar
+*/
+import java.lang.Math;
+class SquareRoot
+{
+    public static void main(String args[])
+	{
+		double x=5; //Declaration and initialization
+		double y;	//Simple declaration
+		y=Math.sqrt(x);
+		System.out.println("Square Root of X="+y);
+
+	}
+}
